ef9a529bc7183e7e3f627ec084580282c87e0226
lautaromessa <tutimessa@hotmail.com>
2021-06-08 19:30:50 -0300

    agregando register y login

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require ('express');
-const path = require ('path')
+const express = require('express');
+const path = require('path')
 
-const app = express ();
+const app = express();
 
 const publicPath = path.resolve(__dirname, './public')
-app.use( express.static(publicPath))
+app.use(express.static(publicPath))
+
+const viewsPath = path.resolve(__dirname, './views')
+app.use(express.static(viewsPath))
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
@@ -12,4 +15,14 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
+
+
